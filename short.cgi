bee02c9551f59719a7ea65c7b912db84bc38f763
Eddie Welker <eddie.welker@gmail.com>
2012-05-24 16:27:55 -0400

    finished cgi, updated short.py to look for a relative ini file.

---
+++ short.cgi
@@ -0,0 +1,29 @@
+#!/home/welkere/env/short/bin/python
+# vim:fileencoding=utf-8
+"""
+Url shortener cgi
+"""
+
+import sys, os, cgi
+sys.stderr = sys.stdout
+print "Content-type: text/plain\n\n"
+
+os.environ['VIRTUAL_ENV'] = '/home/welkere/env/short'
+venv = '/home/welkere/env/info/bin/activate_this.py'
+execfile(venv, dict(__file__=venv))
+
+_project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, _project_dir)
+sys.path.insert(0, '/home/welkere/python/short')
+sys.path.insert(0, os.path.dirname(_project_dir))
+
+from short import shorten_url
+form = cgi.FieldStorage()
+url = form.getvalue('url')
+if url is not None:
+    short = shorten_url(url)
+
+    print short['url']
+else:
+    print "ERROR: no url entered"
--- short.py
+++ short.py
@@ -4,9 +4,10 @@
 Shorten public urls using Bit.ly
 """
 try:
+    import os
     import ConfigParser
-    import bitly_api
     import argparse
+    import bitly_api
     from urlparse import urlparse
 except ImportError, e:
     raise Exception('Required module missing: %s' % e.args[0])
@@ -25,12 +26,13 @@ def read_ini():
     """Read the ini file to get things like the bitly api key, etc."""
     try:
         conf = ConfigParser.ConfigParser()
-        conf.readfp(open('short.ini'))
+        filename = os.path.join(os.path.dirname(__file__), 'short.ini')
+        conf.readfp(open(filename))
         apikey = conf.get('bitly', 'api_key')
         return apikey
 
-    except IOError:
-        print "Sorry, couldn't read the short.ini file"
+    except IOError, e:
+        print "Sorry, couldn't read the short.ini file, %s" % e
 
 def parse_args():
     """Parse command line arguments"""
@@ -40,10 +42,15 @@ def parse_args():
     return args.url.geturl()
 
 
-
-if __name__ == '__main__':
-    url = parse_args()
+def shorten_url(url):
+    """Main method to read ini, get connection, then shorten url"""
     apikey = read_ini()
     bitly = get_bitly_connection(apikey)
     short = bitly.shorten(url)
+    return short
+
+
+if __name__ == '__main__':
+    url = parse_args()
+    short = shorten_url(url)
     print "Your shortened version of\n\n%s\nis\n%s\n" % (url, short['url'])
